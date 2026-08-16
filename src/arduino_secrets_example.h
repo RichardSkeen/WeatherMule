e703c8a3f8dd788b931eb5ddfc16c35d630a95fe
Richard Skeen <richard@kaweahsoft.com>
2026-08-15 17:38:15 -0700

    added secrets example file

---
+++ src/arduino_secrets_example.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// WiFi
+#define SECRET_SSID "<SSID here>"
+#define SECRET_PASS "<WiFi password here>"
+
+// Ambient Weather keys  (ROTATE THESE after posting them publicly)
+#define AMBIENT_API_KEY "<API key here>"
+#define AMBIENT_APP_KEY "<App Key here>"
